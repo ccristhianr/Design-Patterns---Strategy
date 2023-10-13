6be96c5ca6f3022bfc603ab7494d1f215400d81c
ccristhianr <128552808+ccristhianr@users.noreply.github.com>
2023-10-13 20:10:22 -0300

    first commit

---
+++ Geometrica.java
@@ -0,0 +1,15 @@
+public class Geometrica implements ICalcMedia {
+    @Override
+    public double CalculaMedia(double p1, double p2) {
+        return Math.sqrt(p1 * p2);
+    }
+
+    @Override
+    public String Situacao(double p1, double p2) {
+        if (CalculaMedia(p1, p2) > 7.0) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
